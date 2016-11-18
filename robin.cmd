d1724fd3954dc4cd4ac2ec13f76adea3e7be66ea
thunderbird84 <robin.github@gmail.com>
2016-11-18 07:48:10 +0700

    build tools

--- robin.cmd
+++ robin.cmd
@@ -3,6 +3,7 @@
 IF [%1]==[] call:usage
 IF "%1" == "install" call:install
 IF "%1" == "reinstall" call:reinstall
+IF "%1" == "sync" call:sync
 
 :clean
 goto:eof
@@ -18,6 +19,9 @@ echo "%~dp0\tools\NuGet\nuget.exe"
 call  "%~dp0\tools\NuGet\nuget.exe" install -excludeversion -outputdirectory packages
 goto:eof
 
+:sync
+"%~dp0packages\MSBuild\tools\Windows\MSBuild.exe" /target:Sync
+goto:eof
 :usage
 echo cmd:  robin [install,clean]
 goto:eof
